2254930a1fbd264e4895eb051ec332dc52a49b33
Joaquim Rocha <joaquim.rocha@daimler.com>
2020-08-06 23:33:23 +0100

    Add output

--- beast/beast_get.sh
+++ beast/beast_get.sh
@@ -13,4 +13,4 @@ then
 	REQUESTS=$1
 fi
 
-beast run -n $REQUESTS -c $CONCURRENT -config config.json -data ../test_data.csv apps_get.yaml
+beast run -n $REQUESTS -c $CONCURRENT -config config.json -data ../test_data.csv -output get_$CONCURRENT.csv apps_get.yaml
